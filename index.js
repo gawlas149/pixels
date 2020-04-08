4db515cedd9f0c758cea71151a2802c89f3335c2
filip <gawlas149@gmail.com>
2020-04-08 17:44:55 +0200

    add evemts

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ app.use(cors());
 app.use(express.static("app"));
 
 function startServer() {
+  let clients = [];
+
   app.get("/db", (req, res) => {
     const file = fs.readFileSync("db");
     res.send(file.toString());
@@ -30,6 +32,31 @@ function startServer() {
     fs.writeFileSync("db", data);
 
     res.sendStatus(200);
+
+    const event = { color, index };
+    for (let i = 0; i < clients.length; i++) {
+      clients[i].res.write(`data: ${JSON.stringify(event)}\n\n`);
+    }
+  });
+
+  app.get("/db/test", (req, res) => {
+    const headers = {
+      "Content-Type": "text/event-stream",
+      Connection: "keep-alive",
+      "Cache-Control": "no-cache",
+    };
+    res.writeHead(200, headers);
+
+    const clientId = Date.now();
+    const newClient = {
+      id: clientId,
+      res,
+    };
+    clients.push(newClient);
+
+    req.on("close", () => {
+      clients = clients.filter((c) => c.id !== clientId);
+    });
   });
 
   app.listen(port, () =>
